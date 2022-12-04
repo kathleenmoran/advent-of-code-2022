067ea6a559b04072228fc0cf9afded3f22528c22
Kathleen Moran <kathleenmoran10@gmail.com>
2022-12-04 02:21:41 -0500

    Add solution to day 4

---
+++ day_4/part_2_solution.rb
@@ -0,0 +1,15 @@
+def overlaps?(pair)
+  pair = pair.split(',').map { |range| range.split('-').map(&:to_i) }
+  pair[0].any? { |n| n.between?(pair[1][0], pair[1][1]) } ||
+    pair[1].any? { |n| n.between?(pair[0][0], pair[0][1]) }
+end
+
+def total_overlapping_pairs
+  input = File.open('input.txt')
+
+  input.readlines.reduce(0) do |total, pair|
+    overlaps?(pair) ? total + 1 : total
+  end
+end
+
+puts "Total completely overlapping pairs: #{total_overlapping_pairs}"
