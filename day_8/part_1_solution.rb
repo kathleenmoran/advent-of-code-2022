c0129372ba7452c5097a7a479aacc375a5b1681d
Kathleen Moran <kathleenmoran10@gmail.com>
2022-12-08 03:48:03 -0500

    Add solution for day 8

---
+++ day_8/part_1_solution.rb
@@ -0,0 +1,28 @@
+def visible_trees_count
+  forest = File.open('input.txt').readlines.map { |line| line.chomp.split('').map(&:to_i) }
+  trans_forest = forest.transpose
+
+  visible_count = 0
+  forest.each_with_index do |row, r|
+    row.each_with_index.count do |_tree, c|
+      visible_count += 1 if visible?(r, c, forest, trans_forest)
+    end
+  end
+
+  visible_count
+end
+
+def visible?(row, col, forest, trans_forest)
+  on_edge?(row, col, forest) || interior_visible?(row, col, forest, trans_forest)
+end
+
+def on_edge?(row, col, forest)
+  [row, col].include?(0) || [row, col].include?(forest.length - 1)
+end
+
+def interior_visible?(row, col, forest, trans_forest)
+  [forest[row][0...col], forest[row][col + 1..], trans_forest[col][0...row], trans_forest[col][row + 1..]]
+    .map(&:max).any? { |tree| tree < forest[row][col] }
+end
+
+puts "Number of visible trees: #{visible_trees_count}"
---
+++ day_8/part_2_solution.rb
@@ -0,0 +1,33 @@
+def max_scenic_score
+  forest = File.open('input.txt').readlines.map { |line| line.chomp.split('').map(&:to_i) }
+  trans_forest = forest.transpose
+
+  max_scenic_score = 0
+  forest.each_with_index do |row, r|
+    row.each_with_index.count do |_tree, c|
+      max_scenic_score = [max_scenic_score, scenic_score(r, c, forest, trans_forest)].max
+    end
+  end
+
+  max_scenic_score
+end
+
+def scenic_score(row, col, forest, trans_forest)
+  [forest[row][0...col].reverse, forest[row][col + 1..], trans_forest[col][0...row].reverse, trans_forest[col][row + 1..]]
+    .map { |trees| visible_trees_count(trees, forest[row][col]) }.reduce(:*)
+end
+
+def visible_trees_count(trees, target_tree)
+  count = 0
+  prev_tree = -1
+  trees.each do |tree|
+    break if target_tree <= prev_tree
+
+    count += 1
+    prev_tree = tree
+  end
+
+  count
+end
+
+puts "Maximum scenic score #{max_scenic_score}"
